b1b0739d6c5c530e87ea085e422ac3ff7bf038f3
Herllon Cardoso <cardoso.herllon@gmail.com>
2021-01-08 20:41:06 -0300

    refactor: remove duplicated code

--- src/presentation/routers/login-router.spec.js
+++ src/presentation/routers/login-router.spec.js
@@ -1,33 +1,34 @@
 class LoginRouter {
-  route(httpRequest) {
-    if (!httpRequest.body.email || !httpRequest.body.password) {
+  route (httpRequest) {
+    const { email, password } = httpRequest.body
+    if (!email || !password) {
       return {
-        statusCode: 400,
-      };
+        statusCode: 400
+      }
     }
   }
 }
 
-describe("Login Router", () => {
-  test("Should return 400 if no email is provided", () => {
-    const sut = new LoginRouter();
+describe('Login Router', () => {
+  test('Should return 400 if no email is provided', () => {
+    const sut = new LoginRouter()
     const httpRequest = {
       body: {
-        password: "any password",
-      },
-    };
-    const httpResponse = sut.route(httpRequest);
-    expect(httpResponse.statusCode).toBe(400);
-  });
+        password: 'any password'
+      }
+    }
+    const httpResponse = sut.route(httpRequest)
+    expect(httpResponse.statusCode).toBe(400)
+  })
 
-  test("Should return 400 if no password is provided", () => {
-    const sut = new LoginRouter();
+  test('Should return 400 if no password is provided', () => {
+    const sut = new LoginRouter()
     const httpRequest = {
       body: {
-        email: "any@email.com",
-      },
-    };
-    const httpResponse = sut.route(httpRequest);
-    expect(httpResponse.statusCode).toBe(400);
-  });
-});
+        email: 'any@email.com'
+      }
+    }
+    const httpResponse = sut.route(httpRequest)
+    expect(httpResponse.statusCode).toBe(400)
+  })
+})
