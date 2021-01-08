ac0e203f934eb987354a8a29d33e4bac8d0221f3
Herllon Cardoso <cardoso.herllon@gmail.com>
2021-01-08 20:39:16 -0300

    test:ensure LoginRouter returns 400 if no password is provided

--- src/presentation/routers/login-router.spec.js
+++ src/presentation/routers/login-router.spec.js
@@ -1,22 +1,33 @@
 class LoginRouter {
-  route (httpRequest) {
-    if (!httpRequest.body.email) {
+  route(httpRequest) {
+    if (!httpRequest.body.email || !httpRequest.body.password) {
       return {
-        statusCode: 400
-      }
+        statusCode: 400,
+      };
     }
   }
 }
 
-describe('Login Router', () => {
-  test('Should return 400 if no email is provided', () => {
-    const sut = new LoginRouter()
+describe("Login Router", () => {
+  test("Should return 400 if no email is provided", () => {
+    const sut = new LoginRouter();
     const httpRequest = {
       body: {
-        password: 'any password'
-      }
-    }
-    const httpResponse = sut.route(httpRequest)
-    expect(httpResponse.statusCode).toBe(400)
-  })
-})
+        password: "any password",
+      },
+    };
+    const httpResponse = sut.route(httpRequest);
+    expect(httpResponse.statusCode).toBe(400);
+  });
+
+  test("Should return 400 if no password is provided", () => {
+    const sut = new LoginRouter();
+    const httpRequest = {
+      body: {
+        email: "any@email.com",
+      },
+    };
+    const httpResponse = sut.route(httpRequest);
+    expect(httpResponse.statusCode).toBe(400);
+  });
+});
