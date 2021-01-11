d690bb28e4bb88d82291422ce3ec9aad0230dfc5
Herllon Cardoso <cardoso.herllon@gmail.com>
2021-01-11 20:45:32 -0300

    test: ensure Encrypter throws if no params are provided

--- src/utils/helpers/encrypter.js
+++ src/utils/helpers/encrypter.js
@@ -1,7 +1,11 @@
 const bcrypt = require('bcrypt')
+const MissingParamError = require('../errors/missing-param-error')
 
 module.exports = class Encrypter {
   async compare (value, hashedValue) {
+    if (!value || !hashedValue) {
+      throw new MissingParamError('value')
+    }
     const isValid = bcrypt.compare(value, hashedValue)
     return isValid
   }
--- src/utils/helpers/encrypter.spec.js
+++ src/utils/helpers/encrypter.spec.js
@@ -1,5 +1,6 @@
 const bcrypt = require('bcrypt')
 const Encrypter = require('./encrypter')
+const MissingParamError = require('../errors/missing-param-error')
 
 const makeSut = () => {
   return new Encrypter()
@@ -11,16 +12,24 @@ describe('Encrypter', () => {
     const isValid = await sut.compare('any_value', 'hashed_value')
     expect(isValid).toBe(true)
   })
+
   test('Should return false if bcrypt returns true', async () => {
     const sut = makeSut()
     bcrypt.isValid = false
     const isValid = await sut.compare('any_value', 'hashed_value')
     expect(isValid).toBe(false)
   })
+
   test('Should call Encrypter with correct params', async () => {
     const sut = makeSut()
     await sut.compare('any_value', 'hashed_value')
     expect(bcrypt.value).toBe('any_value')
     expect(bcrypt.hashedValue).toBe('hashed_value')
+  })
+
+  test('Should throw if no params are provided', async () => {
+    const sut = makeSut()
+    expect(sut.compare()).rejects.toThrow(new MissingParamError('value'))
+    expect(sut.compare('any_value')).rejects.toThrow(new MissingParamError('value'))
   })
 })
